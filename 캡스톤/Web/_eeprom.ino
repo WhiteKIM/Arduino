a49d51b20400ac2bf74d02dd47c134dd78c952fc
WhiteKIM <9rudrb40@naver.com>
2022-05-26 20:16:21 +0900

    Delete 캡스톤/Web directory

--- 캡스톤/Web/_eeprom.ino
+++
@@ -1,9 +0,0 @@
-#include <EEPROM.h>
-
-#define ssid_address 981
-#define ssid_password 1001
-#define ssid_length_address 1021
-#define ssid_length_password 1022
-
-char memory[21] = "";
-String data("Arduino_IOT");
--- 캡스톤/Web/readme.md
+++
@@ -1,9 +0,0 @@
-# Arduino Web Project
-
-##
-### Notice
-캡스톤 프로젝트와 합쳐질 예정입니다
-##
-### 참고자료
-https://eu4ng.tistory.com/22
-https://turtleshell.kr/66
